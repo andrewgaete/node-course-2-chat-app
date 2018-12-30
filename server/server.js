b7d8060fce9b162ab79bf11d42e3f1f47a38c780
Andrew Gaete <andrewgaete@andrews-imac.lan>
2018-12-30 17:28:31 -0600

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+socket.emit('newMessage', {
+  from: 'Admin',
+  text: 'Welcome to the chat app',
+  created: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+  from: 'Admin',
+  text: 'New user joined',
+  created: new Date().getTime()
+});
+
 socket.on('createMessage', (message) => {
   console.log('CreateMessage', message);
   io.emit('newMessage', {
@@ -21,6 +33,11 @@ socket.on('createMessage', (message) => {
     text: message.text,
     createdAt: new Date().getTime()
   });
+  // socket.broadcast.emit('newMessage', {
+  //   from: message.from,
+  //   text: message.text,
+  //   createdAt: new Date().getTime
+  // });
 });
 
   socket.on('disconnect', () => {
